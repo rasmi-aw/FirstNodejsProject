4fa3c9e26e00f332bd6b41a091abb2e57b3154af
rasmi <abdelwadoudrasmi@gmail.com>
2022-11-28 13:55:15 +0100

    Added html page and made the server return it

--- http.js
+++ http.js
@@ -1,10 +1,15 @@
 let http = require('http');
-
+let fs = require("fs");
 //First js server to listen to port 80
 http
     .createServer()
     .on('request', (req, res) => {
-        res.writeHead(200, "Congrats man")
-        res.end("rasmi");
+        res.writeHead(200, {
+            "Content-type": "text/html; charset= UTF-8"
+        })
+        fs.readFile("index.html", (err, data) => {
+            if (!err)
+                res.end(data);
+        });
     })
-    .listen(80);
+    .listen(81);

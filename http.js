a39bd8fde1c31b5506cf082eab302c81df9b71bc
rasmi <abdelwadoudrasmi@gmail.com>
2022-11-29 13:19:07 +0100

    Get request param and replace a value in html file

--- http.js
+++ http.js
@@ -1,15 +1,18 @@
-let http = require('http');
-let fs = require("fs");
+let http = require('http')
+let fs = require("fs")
+let url = require("url")
 //First js server to listen to port 80
 http
     .createServer()
     .on('request', (req, res) => {
-        res.writeHead(200, {
+        let query = url.parse(req.url, true).query
+        res.writeHead(200, "Yes man", {
             "Content-type": "text/html; charset= UTF-8"
         })
         fs.readFile("index.html", (err, data) => {
             if (!err)
-                res.end(data);
+                data = data.toString().replace("name", query.name)
+            res.end(data)
         });
     })
-    .listen(81);
+    .listen(80)

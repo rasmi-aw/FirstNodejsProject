ae6665ea7c20cb9ac8d86596fcf1bb34c682de55
Abdel Wadoud <88406384+rasmi-aw@users.noreply.github.com>
2022-12-08 10:48:53 +0100

    update

--- http.js
+++ http.js
@@ -2,16 +2,28 @@ let http = require('http')
 let fs = require("fs")
 let url = require("url")
 //First js server to listen to port 80
-http
-    .createServer((req, res) => {
-        let query = url.parse(req.url, true).query
-        res.writeHead(200, "Yes man", {
-            "Content-type": "text/html; charset= UTF-8"
+
+
+let Server = {
+    listen: (port) => {
+        let server = http.createServer((req, resp) => {
+            let query = url.parse(req.url)
+            resp.writeHead(200, "Yes man", {
+                "Content-type": "text/html; charset= UTF-8"
+            })
+            let data = "Nada"
+            if (req.url === "/")
+                fs.readFile("index.html", (err, data) => {
+                    if (!err) {
+                        console.log("Yes")
+                        data = data.toString().replace("name", query.name).replace(ppp, "root")
+                        resp.end(data)
+                    } else resp.end()
+                })
+
         })
-        fs.readFile("index.html", (err, data) => {
-                if (!err)
-                    data = data.toString().replace("name", query.name)
-                res.end(data)
-            }
-        )
-    }).listen(80)
+        server.listen(port)
+    }
+};
+//
+Server.listen(80);
